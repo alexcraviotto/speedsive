27a004b63b2c6dccd28db4ced54b58967c86158c
Alex Craviotto <craviottoalex@gmail.com>
2022-07-16 16:33:18 +0200

    feat: Logger established

---
+++ speedsive/logger.py
@@ -0,0 +1,11 @@
+import logging
+import yaml
+import logging.config
+
+with open('speedsive/logs/logging.yml','rt') as f:
+        config=yaml.safe_load(f.read())
+        f.close()
+
+logging.config.dictConfig(config)
+
+logger = logging.getLogger("speedsive")
