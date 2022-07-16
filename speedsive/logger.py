ae6b6bb5c4c597e687b1fe26291f85f4c8a313ff
Alex Craviotto <craviottoalex@gmail.com>
2022-07-16 18:00:06 +0200

    fix: bad path to logging config file

--- speedsive/logger.py
+++ speedsive/logger.py
@@ -2,10 +2,12 @@ import logging
 import yaml
 import logging.config
 
-with open('speedsive/logs/logging.yml', 'rt') as f:
+with open('speedsive/logging.yml', 'rt') as f:
     config = yaml.safe_load(f.read())
     f.close()
 
 logging.config.dictConfig(config)
 
 logger = logging.getLogger("speedsive")
+
+print("hola")
