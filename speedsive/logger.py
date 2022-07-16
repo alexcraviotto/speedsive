52ef26835533a3382c7827ad9a5964b6c3387207
Alex Craviotto <craviottoalex@gmail.com>
2022-07-16 17:23:31 +0200

    build: mypy configuration file added

--- speedsive/logger.py
+++ speedsive/logger.py
@@ -2,9 +2,9 @@ import logging
 import yaml
 import logging.config
 
-with open('speedsive/logs/logging.yml','rt') as f:
-        config=yaml.safe_load(f.read())
-        f.close()
+with open('speedsive/logs/logging.yml', 'rt') as f:
+    config = yaml.safe_load(f.read())
+    f.close()
 
 logging.config.dictConfig(config)
 
