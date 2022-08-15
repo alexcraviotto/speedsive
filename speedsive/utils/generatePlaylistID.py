250c063c3e84dfbf275bb3761ff9b87e00bcd933
Alex Craviotto <alexcraviotto@pm.me>
2022-08-15 21:24:13 +0200

    feature: Read playlistsID file

---
+++ speedsive/utils/generatePlaylistID.py
@@ -0,0 +1,19 @@
+from logger import logger
+
+def generatePlaylistID():
+    try:
+        with open('././speedsive/playlists.txt','r+') as fp:
+            lines = fp.readlines()
+            fp.seek(0)
+            fp.truncate()
+            result = lines[0]
+            try:
+                lines[0] = ""
+                fp.writelines(lines[1:])
+            except:
+                logger.warn('You need to add more playlists ID on the file')
+            return result
+    except:
+        logger.error('Error on playlistID generation')
+
+
